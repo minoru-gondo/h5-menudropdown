bdfe19633feb2d832099e8973845f196f4d1c5f5
thr0w <josefernandotolentino@gmail.com>
2015-08-07 10:11:29 -0300

    menudropdown

---
+++ test/template/app/exemplo.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var Menudropdown = require('../../../src/h5-menudropdown.js');
+
+window.hsession = {
+    language: 'pt_br'
+};
+
+var mock_store = {
+    items: [
+        {
+            caption: "QuickEdit",
+            onClick: function clica() {
+                alert("QuickEdit");
+            }
+        },
+        {
+            caption: "Quick Docs",
+            onClick: function clica() {
+                alert("Quick Docs");
+            }
+        }
+    ]
+};
+
+var Exemplo = React.createClass({
+    render: function () {
+        return React.createElement(Menudropdown, {
+            store: mock_store
+        });
+    }
+});
+
+React.render(<Exemplo/>, document.getElementById('app'));
