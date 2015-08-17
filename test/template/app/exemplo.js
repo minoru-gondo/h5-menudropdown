fa6f8e305ccaf2e6620fbfbb80096e02b4d3c0c9
José Augusto <joseneto.design@gmail.com>
2015-08-17 11:40:42 -0300

    Correção NPM link

--- test/template/app/exemplo.js
+++ test/template/app/exemplo.js
@@ -22,8 +22,103 @@ window.hsession = {
 //    ]
 //};
 
+//var mock_store = {
+//    menu: [___fields___]
+//};
+
 var mock_store = {
-    menu: [___fields___]
+    actions: {
+        voltar: {
+            labelText: 'Voltar',
+            mode: 'visible',
+            kind: 'secondary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Sair da tela de edição do paciente'
+        },
+        tirar_foto: {
+            labelText: 'Tirar Foto',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_1: {
+            labelText: 'Tirar Foto 1',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_2: {
+            labelText: 'Tirar Foto 2',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_3: {
+            labelText: 'Tirar Foto 3',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_4: {
+            labelText: 'Tirar Foto 4',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_5: {
+            labelText: 'Tirar Foto 5',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_6: {
+            labelText: 'Tirar Foto 6',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_7: {
+            labelText: 'Tirar Foto 7',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        },
+        tirar_foto_8: {
+            labelText: 'Tirar Foto 8',
+            mode: 'visible',
+            kind: 'tertiary',
+            run: function () {
+                alert("OK");
+            },
+            hintText: 'Tirar foto do paciente'
+        }
+    }
 };
 
 var Exemplo = React.createClass({
@@ -46,8 +141,9 @@ var Exemplo = React.createClass({
 
         return React.createElement('div', {}, ['exemplo menu drop down',
                    React.createElement(Menudropdown, {
+                key: "EXEMPLO",
                 icon: 'fa fa-ellipsis-v fa-2x',
-                menu: mock_store.menu
+                actions: mock_store.actions
             })
 
            ]);
